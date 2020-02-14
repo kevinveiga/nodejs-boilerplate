49c0d0a7d7721fae52ce7755541fce85c8514a69
Kevin Veiga <kevin.veiga@gmail.com>
2020-02-14 14:27:31 -0300

    update

--- src/app.js
+++ src/app.js
@@ -26,11 +26,11 @@ const apiCache1Day = axios.create({
     headers: { 'Cache-Control': 'no-cache' }
 });
 
-const apiCache30Day = axios.create({
-    adapter: cacheAdapterEnhancer(axios.defaults.adapter, { maxAge: 1000 * 60 * 60 * 24 * 30, max: 300000000 }),
-    baseURL: '/',
-    headers: { 'Cache-Control': 'no-cache' }
-});
+// const apiCache30Day = axios.create({
+//     adapter: cacheAdapterEnhancer(axios.defaults.adapter, { maxAge: 1000 * 60 * 60 * 24 * 30, max: 300000000 }),
+//     baseURL: '/',
+//     headers: { 'Cache-Control': 'no-cache' }
+// });
 
 let interval = null;
 
@@ -53,13 +53,91 @@ app.get('/', (req, res) => {
 });
 
 // FUNCTION
-const getApi = async (socket) => {
+/**
+ * @description Busca API da CDI do Banco Central.
+ */
+const getApiCdi = async () => {
+    try {
+        const result = await apiCache1Day.get('https://api.bcb.gov.br/dados/serie/bcdata.sgs.4389/dados/ultimos/2?formato=json');
+
+        const variation = parseFloat(result.data[1].valor) - parseFloat(result.data[0].valor);
+
+        return { value: parseFloat(result.data[1].valor).toFixed(2), operator: variation < 0 ? '-' : '+', variation: variation };
+    } catch (error) {
+        console.error(`Error getApiCDI: ${error.code}`);
+    }
+
+    return null;
+};
+
+/**
+ * @description Busca API da Infomoney.
+ * (Obs: caso ocorra algum erro, provavelmente chegou no limite de requisições permitidas da Infomoney,
+ * pode ser usado os dados da HG Brasil no lugar da Infomoney).
+ */
+const getApiInfomoney = async () => {
+    const valuesToRemove = ['ABEV3', 'GGBR4', 'IFIX', 'ITUB4', 'MGLU3', 'PETR4', 'VALE3'];
+
     try {
         const result = await axios.get('https://api.infomoney.com.br/ativos/ticker?type=json&_=1143');
 
-        socket.emit('msg', JSON.stringify(result.data));
+        const newResult = result.data.filter((elem) => {
+            return valuesToRemove.indexOf(elem.Name) === -1;
+        });
+
+        return newResult;
     } catch (error) {
-        console.error(`Error: ${error.code}`);
+        console.error(`Error getApiInfomoney: ${error.code}`);
+    }
+
+    return null;
+};
+
+/**
+ * @description Busca API da Poupança do Banco Central.
+ */
+const getApiPoupanca = async () => {
+    try {
+        const result = await apiCache1Day.get('https://api.bcb.gov.br/dados/serie/bcdata.sgs.195/dados/ultimos/2?formato=json');
+
+        const variation = parseFloat(result.data[1].valor) - parseFloat(result.data[0].valor);
+
+        return { value: parseFloat(result.data[1].valor).toFixed(2), operator: variation < 0 ? '-' : '+', variation: variation };
+    } catch (error) {
+        console.error(`Error getApiPoupanca: ${error.code}`);
+    }
+
+    return null;
+};
+
+/**
+ * @description Busca API da SELIC do Banco Central.
+ */
+const getApiSelic = async () => {
+    try {
+        const result = await apiCache1Day.get('https://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados/ultimos/2?formato=json');
+
+        const variation = parseFloat(result.data[1].valor) - parseFloat(result.data[0].valor);
+
+        return { value: parseFloat(result.data[1].valor).toFixed(2), operator: variation < 0 ? '-' : '+', variation: variation };
+    } catch (error) {
+        console.error(`Error getApiSELIC: ${error.code}`);
+    }
+
+    return null;
+};
+
+/**
+ * @description Busca todas as API's.
+ * @param {object} socket Objeto do socket.io.
+ */
+const getApis = async (socket) => {
+    try {
+        const [resultCdi, resultInfomoney, resultPoupanca, resultSelic] = await Promise.all([getApiCdi(), getApiInfomoney(), getApiPoupanca(), getApiSelic()]);
+
+        socket.emit('quotationData', JSON.stringify({ bolsa: resultInfomoney, cdi: resultCdi, poupanca: resultPoupanca, selic: resultSelic }));
+    } catch (error) {
+        console.error(`Error getApis: ${error.code}`);
     }
 };
 
@@ -99,7 +177,7 @@ io.on('connection', (socket) => {
 
         getInfo();
 
-        getApi(socket);
+        getApis(socket);
 
         if (interval) {
             clearInterval(interval);
@@ -111,7 +189,7 @@ io.on('connection', (socket) => {
 
             getInfo();
 
-            getApi(socket);
+            getApis(socket);
         }, 60000);
 
         socket.on('msg', (msg) => {
